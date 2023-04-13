9b28276cc8f93b87ce35b620a7fbdae4a87f4348
Phillip Kruger <phillip.kruger@gmail.com>
2023-04-13 15:13:28 +1000

    Added
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

---
+++ src/test/java/com/example/GreetingResourceTest.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloTestEndpoint() {
+        given()
+          .when().get("/hello/Test")
+          .then()
+             .statusCode(200)
+             .body(is("Hello Test"));
+    }
+
+    @Test
+    public void testHelloWithSpaceEndpoint() {
+        given()
+          .when().get("/hello/Test Space")
+          .then()
+             .statusCode(200)
+             .body(is("Hello Test Space"));
+    }
+    
+    @Test
+    public void testHelloAnotherEndpoint() {
+        given()
+          .when().get("/hello/Another")
+          .then()
+             .statusCode(200)
+             .body(is("Hello Another"));
+    }
+    
+}
